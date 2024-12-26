a7c6d6bb58189f19a4603e0c7ad2c8fa0f35179f
lordguido <rodfrei@gmail.com>
2024-12-26 15:38:35 -0300

    adiciona configurações do ESLint e Prettier no VSCode, ajusta formatação e melhora o código do servidor

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,26 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import prettier from "eslint-config-prettier";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     languageOptions: {
       globals: globals.node,
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
     rules: {
-      "no-console": "off",
-      "prettier/prettier": "error",
+      'no-console': 'off',
+      'prettier/prettier': 'error',
     },
   },
   pluginJs.configs.recommended,
   prettier,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+  },
 ];
--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
-// servidor basico
-const http=require('http');
+// servidor
+const http = require('http');
 
-const server=http.createServer((req,res)=>{
-res.writeHead(200,{'Content-Type':'text/plain'});
-if(req.url=='/'){
-    res.end('Hello World')
-}else
-if(req.url=='/test'){
-    res.end('Test Page')
-}else{
-res.end('Page Not Found')}
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  if (req.url == '/') {
+    console.log('Hello World');
+    res.end('Hello World');
+  } else if (req.url == '/test') {
+    res.end('Test Page');
+  } else {
+    res.end('Page Not Found');
+  }
 });
 
-server.listen(3000,()=>{
-console.log('Server is running on port 3000')})
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
