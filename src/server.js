e07d114d3d228f25f610f98cf2791453496a5672
lordguido <rodfrei@gmail.com>
2024-12-26 18:01:33 -0300

    adiciona suporte a variáveis de ambiente com dotenv e ajusta scripts de inicialização

--- src/server.js
+++ src/server.js
@@ -1,5 +1,22 @@
-// servidor
-const http = require('http');
+// SERVIDOR HTTP
+import dotenv from 'dotenv';
+import http from 'http';
+
+const envFiles = {
+  prod: '.env.prod',
+  dev: '.env.dev',
+};
+
+const envPath = envFiles[process.env.NODE_ENV] || envFiles.dev;
+dotenv.config({ path: envPath });
+
+const port = process.env.APP_PORT;
+const version = process.env.APP_VERSION;
+
+if (!port || !version) {
+  console.error('Required environment variables are missing');
+  process.exit(1);
+}
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -13,6 +30,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+server.listen(port, () => {
+  console.log(`Server is running on port ${port} and version ${version}`);
 });
