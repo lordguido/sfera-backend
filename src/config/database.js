321f1a9156a315835e51fe92281466eded178b52
lordguido <rodfrei@gmail.com>
2024-12-27 16:13:36 -0300

    refatora configuração do banco de dados, adiciona suporte a múltiplos ambientes e cria migração para a tabela de usuários

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,30 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export default {
-  dialect: 'postgres',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  logging: process.env.NODE_ENV === 'dev' ? console.log : false,
-  define: {
-    timestamps: true,
-    underscored: true,
+  development: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    logging: process.env.NODE_ENV === 'dev' ? console.log : false,
+  },
+  test: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.TEST_DB_NAME || 'test_db',
+    logging: false,
+  },
+  production: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    logging: false,
   },
 };
