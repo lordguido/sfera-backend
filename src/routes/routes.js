1c96b8eb0892490dc67627e1ecf86cd8b87c673c
lordguido <rodfrei@gmail.com>
2024-12-26 21:08:35 -0300

    adiciona o Express e refatora o servidor para usar roteamento modular

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,17 @@
-import url from 'url';
+import { Router } from 'express';
 
-export default function handleRoutes(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+const router = Router();
 
-  const parsedUrl = url.parse(req.url, true);
-  const route = parsedUrl.pathname;
+router.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-  const routes = {
-    '/': () => 'Hello World!',
-    '/test': () => 'Test Page',
-  };
+router.get('/test', (req, res) => {
+  res.send('Test Page');
+});
 
-  const response = routes[route] ? routes[route]() : 'Page Not Found';
-  res.end(response);
-}
+router.use((req, res) => {
+  res.status(404).send('Page Not Found');
+});
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,16 +1,27 @@
-import http from 'http';
+import express from 'express';
 import chalk from 'chalk';
 import config from './config/configServer.js';
-import handleRoutes from './routes/routes.js';
+import routes from './routes/routes.js';
 
-const server = http.createServer(handleRoutes);
+const app = express();
 
-server.listen(config.port, () => {
+app.use((req, res, next) => {
+  const formattedDate = new Date().toLocaleString('pt-BR', {
+    dateStyle: 'short',
+    timeStyle: 'medium',
+  });
+  console.log(`[${formattedDate}] => [${req.method}] ${req.url}`);
+  next();
+});
+
+app.use(routes);
+
+app.listen(config.port, () => {
   console.info(
     '\n',
     chalk.green.bold('━━━━━━━━━━━━━━━━━━━━━━━'),
     '\n',
-    chalk.green.bold('  SERVER STARTED  🚀  '),
+    chalk.green.bold('  SERVER STARTED  🟢  '),
     '\n',
     chalk.green.bold('━━━━━━━━━━━━━━━━━━━━━━━'),
     '\n',
