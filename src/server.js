f0f0550ce778687c7eb5d5f9e3f3f26712b167e0
brenoricci@gmail.com <brenoricci@gmail.com>
2025-01-13 12:28:08 -0300

    criação pasta teste

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const HTTP_STATUS = {
   OK: 200,
 };
 const MESSAGES = {
-  SERVER_RUNNING: 'Servidor Express configurado e funcionando!',
+  SERVER_RUNNING: 'teste Breno Servidor Express configurado e funcionando!',
   SERVER_STATUS: (nodeEnv, port) => `Servidor rodando no modo ${nodeEnv} na porta ${port}`,
 };
 
